8ff86b7dcc03b303b4ef9b6a020b7e7380b7e8ec
baninations <baninations@gmail.com>
2023-09-30 11:01:06 +0200

    getUser endpoint

--- index.js
+++ index.js
@@ -171,12 +171,27 @@ app.get(
       });
   }
 );
-//getUser
+// //getUser
+// app.get(
+//   "users/:_id",
+//   passport.authenticate("jwt", { session: false }),
+//   async (res, req) => {
+//     await Users.findOne({ _id: req.params_id })
+//       .then((user) => {
+//         res.json(user);
+//       })
+//       .catch((err) => {
+//         console.error(err);
+//         res.status(500).send("Error: " + err);
+//       });
+//   }
+// );
+
 app.get(
-  "users/:_id",
+  "/users/:_id",
   passport.authenticate("jwt", { session: false }),
-  async (res, req) => {
-    await Users.findOne({ _id: req.params_id })
+  async (req, res) => {
+    await Users.findOne({ _id: req.params._id })
       .then((user) => {
         res.json(user);
       })
