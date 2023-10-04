ba6305a2052b4f37ac9f153797eb07f4643b935c
baninations <baninations@gmail.com>
2023-10-04 11:44:47 +0200

    cors

--- index.js
+++ index.js
@@ -208,13 +208,40 @@ app.put(
   "/users/:_id",
   passport.authenticate("jwt", { session: false }),
   async (req, res) => {
-    let hashedPassword = Users.hashPassword(req.body.Password); // added line zazzy
+    let hashedPassword = Users.hashPassword(req.body.Password);
     await Users.findOneAndUpdate(
       { _id: req.params._id },
       {
         $set: {
           Username: req.body.Username,
-          Password: hashedPassword, // changed this line zazzy
+          Password: hashedPassword,
+          Email: req.body.Email,
+          Birthday: req.body.Birthday,
+        },
+      },
+      { new: true }
+    )
+      .then((updatedUser) => {
+        res.json(updatedUser);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error: " + err);
+      });
+  }
+);
+
+app.put(
+  "/users/:Username",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    let hashedPassword = Users.hashPassword(req.body.Password);
+    await Users.findOneAndUpdate(
+      { _id: req.params.Username },
+      {
+        $set: {
+          Username: req.body.Username,
+          Password: hashedPassword,
           Email: req.body.Email,
           Birthday: req.body.Birthday,
         },
