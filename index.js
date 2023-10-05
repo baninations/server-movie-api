4217914185b2465aa07a45936345f65a8a6fa6f6
baninations <baninations@gmail.com>
2023-10-05 18:12:01 +0200

    ...

--- index.js
+++ index.js
@@ -7,38 +7,6 @@ const express = require("express"),
   Movies = Models.Movie,
   Users = Models.user,
   { check, validationResult } = require("express-validator");
-cors = require("cors");
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-
-app.use(
-  cors({
-    origin: "*",
-  })
-);
-
-// app.use(
-//   cors({
-//     origin: (origin, callback) => {
-//       if (!origin) return callback(null, true);
-//       if (allowedOrigins.indexOf(origin) === -1) {
-//         let message =
-//           "The CORS policy for this application doesn’t allow access from origin " +
-//           origin;
-//         return callback(new Error(message), false);
-//       }
-//       return callback(null, true);
-//     },
-//   })
-// );
 
 let auth = require("./auth")(app); // Login HTML Authentification
 const passport = require("passport"); // JWT Authentification
