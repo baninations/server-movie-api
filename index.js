57c2d958f72957213ec1fe614fb73d83c4c01f03
baninations <baninations@gmail.com>
2023-10-05 17:57:16 +0200

    Added JsDoc

--- index.js
+++ index.js
@@ -8,17 +8,16 @@ const express = require("express"),
   Users = Models.user,
   { check, validationResult } = require("express-validator");
 cors = require("cors");
-let allowedOrigins = [
-  "*",
-  "http://localhost:8080",
-  "http://testsite.com",
-  "http://localhost:1234",
-  "https://localhost:1234",
-  "https://localhost:49838",
-  "http://localhost:49838",
-  "http://localhost:54190",
-  "http://localhost:4200",
-];
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.use(
   cors({
