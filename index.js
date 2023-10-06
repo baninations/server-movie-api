9b7992a3826e9e0b4e3a7dbeff68919c6c4513ce
baninations <baninations@gmail.com>
2023-10-06 13:06:20 +0200

    dos

--- index.js
+++ index.js
@@ -19,21 +19,6 @@ let allowedOrigins = [
   "http://localhost:54190",
 ];
 
-// app.use(
-//   cors({
-//     origin: (origin, callback) => {
-//       if (!origin) return callback(null, true);
-//       if (allowedOrigins.indexOf(origin) === -1) {
-//         let message =
-//           "The CORS policy for this application doesn’t allow access from origin " +
-//           origin;
-//         return callback(new Error(message), false);
-//       }
-//       return callback(null, true);
-//     },
-//   })
-// );
-
 app.use(
   cors({
     origin: "*",
@@ -171,21 +156,6 @@ app.get(
       });
   }
 );
-// //getUser
-// app.get(
-//   "users/:_id",
-//   passport.authenticate("jwt", { session: false }),
-//   async (res, req) => {
-//     await Users.findOne({ _id: req.params_id })
-//       .then((user) => {
-//         res.json(user);
-//       })
-//       .catch((err) => {
-//         console.error(err);
-//         res.status(500).send("Error: " + err);
-//       });
-//   }
-// );
 
 app.get(
   "/users/:_id",
